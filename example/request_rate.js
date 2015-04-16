18c199f949629e3f1bed7c669dba82e9f115dd6a
you21979 <you2197901@gmail.com>
2015-04-16 16:47:37 +0900

    change request rate 0.34 -> 0.2

---
+++ example/request_rate.js
@@ -0,0 +1,27 @@
+var zaif = require('..');
+var api = zaif.PublicApi;
+var Promise = require('bluebird');
+
+var n = 0;
+var r = function(v){
+    console.log(++n);
+    return v;
+}
+
+var w = [
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r),
+    api.lastPrice('btc_jpy').then(r)
+];
+
+Promise.all(w).then(function(){console.log("OK")})
