495e160aaf54ec5ef7f97f48541f6b3f6ad3334c
you21979 <you2197901@gmail.com>
2016-07-04 20:00:26 +0900

    simple-verifyを利用するように変更。sampleにxem/jpy追加

--- example/sample.js
+++ example/sample.js
@@ -5,6 +5,7 @@ var api = zaif.PublicApi;
 Promise.all(
 [
     api.lastPrice('btc_jpy').then(function(v){return ['btc_jpy', v.last_price]}),
+    api.lastPrice('xem_jpy').then(function(v){return ['xem_jpy', v.last_price]}),
     api.lastPrice('mona_jpy').then(function(v){return ['mona_jpy', v.last_price]}),
     api.lastPrice('mona_btc').then(function(v){return ['mona_btc', v.last_price]})
 ]
