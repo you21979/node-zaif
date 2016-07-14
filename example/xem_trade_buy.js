fdc50c7dc03cba9e7c4094e6b5d5f9d3bc98e9e7
you21979 <you2197901@gmail.com>
2016-07-14 16:13:08 +0900

    add sample

---
+++ example/xem_trade_buy.js
@@ -0,0 +1,15 @@
+var zaif = require('..');
+var fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+var main = function(config){
+    var api = zaif.createPrivateApi(config.apikey, config.secretkey, 'tradebot');
+    var pair = "XEM_JPY";
+    var action = "BID";
+    var price = 1;
+    var amount = 1;
+    return api.trade(pair, action, price, amount).then(console.log).catch(console.log)
+}
+
+main(config)
