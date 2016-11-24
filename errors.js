400bf4cc292c6c1e63a3e48a55239fa641fec0c6
you21979 <you2197901@gmail.com>
2016-11-24 18:03:53 +0900

    pkg mod

--- errors.js
+++ errors.js
@@ -3,4 +3,4 @@ var objectutil = require('@you21979/object-util');
 
 module.exports = objectutil.keyMerge({
    HttpApiError : require('@you21979/http-api-error'),
-}, require('request-promise/errors'));
+}, require('limit-request-promise/errors'));
